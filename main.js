df62c6a67f9e5008cd530515cceac7773541d636
Giovanna Jaqueto <gijaqueto1@gmail.com>
2023-07-17 23:30:51 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,40 @@
+const form = document.getElementById('form-agenda');
+const nome = [];
+const telefone = [];
+
+let linhas = '';
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    adicionaLinha();
+    atualizaTabela();
+});
+
+function adicionaLinha() {
+    const inputNome = document.getElementById('nome-contato');
+    const inputTelefone = document.getElementById('telefone-contato');
+
+    if (atividades.includes(inputNome.value)) {
+        alert(`O nome ${inputNome.value} já foi inserido.`);
+    } else {
+
+        nome.push(inputNome.value);
+        telefone.push(parseFloat(inputTelefone.value));
+
+        let linha = '<tr>';
+        linha += `<td> ${inputNome.value}</td>`;
+        linha += `<td> ${inputTelefone.value}</td>`;
+        linha += '</tr>';
+
+        linhas += linha;
+    }
+
+    inputNome.value = '';
+    inputTelefone.value = '';
+}
+
+function atualizaTabela() {
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+}
